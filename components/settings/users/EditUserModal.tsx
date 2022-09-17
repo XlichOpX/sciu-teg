9e0c2ffc14b042194ea1312c3456cfc296b19314
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-09-16 20:42:21 -0400

    Layout users settings tab

---
+++ components/settings/users/EditUserModal.tsx
@@ -0,0 +1,60 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure
+} from '@chakra-ui/react'
+import { Select } from 'chakra-react-select'
+import EditButton from 'components/EditButton'
+
+function EditUserModal() {
+  const { isOpen, onClose, onOpen } = useDisclosure()
+
+  return (
+    <>
+      <EditButton onClick={onOpen} />
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <h2>Editar Usuario #X</h2>
+          </ModalHeader>
+          <ModalCloseButton />
+
+          <ModalBody>
+            <form>
+              <FormControl>
+                <FormLabel>Roles</FormLabel>
+                <Select
+                  placeholder="Seleccionar roles..."
+                  isMulti
+                  options={[
+                    { value: 'admin', label: 'Admin' },
+                    { value: 'plebeyo', label: 'Plebeyo' },
+                    { value: 'don-nadie', label: 'Don Nadie' }
+                  ]}
+                />
+              </FormControl>
+            </form>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button mr={3} onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button colorScheme="blue">Guardar cambios</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
+
+export default EditUserModal
--- pages/configuracion/usuarios.tsx
+++ pages/configuracion/usuarios.tsx
@@ -1,12 +1,21 @@
-import { Heading } from '@chakra-ui/react'
-import { BaseLayout } from 'components'
+import { Divider, Flex } from '@chakra-ui/react'
+import { BaseLayout, SearchInput } from 'components'
 import { Layout } from 'components/settings'
+import CreateUserModal from 'components/settings/users/CreateUserModal'
+import UserList from 'components/settings/users/UserList'
 import { NextPageWithLayout } from 'pages/_app'
 
 const UsersSettings: NextPageWithLayout = () => {
   return (
     <>
-      <Heading as="h2">Usuarios</Heading>
+      <Flex direction={['column', 'row']} align="stretch" justify="space-between" gap={4}>
+        <SearchInput w="auto" placeholder="Buscar usuarios" />
+        <CreateUserModal />
+      </Flex>
+
+      <Divider my={4} />
+
+      <UserList />
     </>
   )
 }
