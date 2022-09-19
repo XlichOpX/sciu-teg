64b14f567b2acfef4699478267b9bf19d309d845
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-09-19 14:47:21 -0400

    Layout general settings page

--- pages/configuracion/general.tsx
+++ pages/configuracion/general.tsx
@@ -1,11 +1,54 @@
-import { Heading } from '@chakra-ui/react'
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  SimpleGrid,
+  VStack
+} from '@chakra-ui/react'
 import { Layout } from 'components/settings'
 import { NextPageWithLayout } from 'pages/_app'
 
 const GeneralSettings: NextPageWithLayout = () => {
   return (
     <>
-      <Heading as="h2">General</Heading>
+      <Heading as="h2" size="md" mb={4}>
+        Datos de la Institución
+      </Heading>
+
+      <VStack as="form" align="stretch" gap={4}>
+        <SimpleGrid columns={[1, 2]} gap={4}>
+          <FormControl>
+            <FormLabel>Nombre</FormLabel>
+            <Input />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>RIF</FormLabel>
+            <Input />
+          </FormControl>
+        </SimpleGrid>
+
+        <FormControl>
+          <FormLabel>Dirección</FormLabel>
+          <Input />
+        </FormControl>
+
+        <SimpleGrid columns={[1, 2]} gap={4}>
+          <FormControl>
+            <FormLabel>Zona</FormLabel>
+            <Input />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Teléfono</FormLabel>
+            <Input />
+          </FormControl>
+        </SimpleGrid>
+
+        <Button colorScheme="blue">Guardar cambios</Button>
+      </VStack>
     </>
   )
 }
