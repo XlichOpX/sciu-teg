7d0b1c42ac8e43b5c53f503a4fd859014686a26b
kurokuro15 <kurokuro15000@gmail.com>
2022-09-21 14:02:36 -0400

    Add Callbacks returning credentials information

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { authorize } from '../../../services/CredentialsProvider'
@@ -7,7 +7,7 @@ const { GITHUB_ID, GITHUB_SECRET } = process.env
 
 if (!GITHUB_ID || !GITHUB_SECRET) throw new Error('Not Github eviroment variables configured')
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   // configure all providers here
   providers: [
     GitHubProvider({
@@ -19,12 +19,25 @@ export const authOptions = {
     CredentialsProvider({
       name: 'Credentials',
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        username: { label: 'Username', type: 'text', placeholder: 'kurokuro15' },
         password: { label: 'Password', type: 'password' }
       },
       authorize
     })
-  ]
+  ],
+  callbacks: {
+    async session({ session, token }) {
+        session.user  = token.user as { id:number, statusId:number, username:string }
+
+      return session 
+    }, 
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user
+      }
+      return token
+    }
+  }
 }
 
 export default NextAuth(authOptions)
