04226006553da7cfc376c02a3806e05269aa8253
kurokuro15 <kurokuro15000@gmail.com>
2022-09-22 14:22:37 -0400

    Add Person CRUD EP

---
+++ pages/api/person/[id].ts
@@ -0,0 +1,59 @@
+import { Person } from '@prisma/client'
+import prisma from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import z from 'zod'
+
+export default async function personHandler(req: NextApiRequest, res: NextApiResponse) {
+  // Validate typeof id
+  const idValidation = z.preprocess((value) => Number(value), z.number().positive())
+
+  const {
+    body,
+    method,
+    query: { id }
+  } = req
+
+  const { success } = idValidation.safeParse(id)
+  if (!success) return res.status(404).send(`Id ${id} Not Allowed`)
+
+  switch (method) {
+    case 'GET':
+      //obtenemos a UNA persona
+      const person: Person | null = await prisma.person.findFirst({
+        include: {
+          address: true,
+          client: true,
+          docType: true,
+          receipts: false,
+          student: true,
+          user: true
+        },
+        where: { id: Number(id) }
+      })
+      if (!person) res.status(404).end(`person not found`)
+      res.status(201).send(person)
+      break
+    case 'PUT':
+      //actualizamos a UNA persona
+      const updatePerson: Person = await prisma.person.update({
+        data: {
+          ...body
+        },
+        where: {
+          id: Number(id)
+        }
+      })
+      if (!updatePerson) res.status(404).end(`person not found`)
+      res.status(201).send(updatePerson || {})
+      break
+    case 'DELETE':
+      //eliminamos a UNA persona
+      const delperson: Person = await prisma.person.delete({ where: { id: Number(id) } })
+      res.status(202).send(delperson)
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT', 'DELETE'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+      break
+  }
+}
---
+++ pages/api/person/index.ts
@@ -0,0 +1,29 @@
+import { Person } from '@prisma/client'
+import prisma from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// GET|POST /api/person
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const { body, method } = req
+
+  switch (method) {
+    case 'GET':
+      //obtenemos TODAS las personas
+      const people: Person[] | null = await prisma.person.findMany()
+      if (!people) res.status(404).end(`People not found`)
+      res.status(200).send(people)
+      break
+    case 'POST':
+      const result = await prisma.student.create({
+        data: {
+          ...body
+        }
+      })
+      res.status(201).send(result)
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+      break
+  }
+}
