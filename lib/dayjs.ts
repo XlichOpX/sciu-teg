66e69319de03d75b7cbd57fe1464ce3545552648
kurokuro15 <kurokuro15000@gmail.com>
2022-10-09 00:34:54 -0400

    Add DayJS Lib for take spanish months

--- lib/dayjs.ts
+++ lib/dayjs.ts
@@ -1,2 +1,24 @@
-export {}
-const dayjs = require('dayjs')
+import dayjs from 'dayjs'
+import 'dayjs/locale/es-us'
+import updateLocale from 'dayjs/plugin/updateLocale'
+
+dayjs.extend(updateLocale)
+
+dayjs.updateLocale('en', {
+  months: [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ]
+})
+
+export default dayjs
