4161c294938c1e7a7fca317bac7c9b4314ec4d3f
kurokuro15 <kurokuro15000@gmail.com>
2022-10-09 00:45:03 -0400

    Fix add Validations responses and remove some comments

--- pages/api/student/[id]/billing.ts
+++ pages/api/student/[id]/billing.ts
@@ -29,23 +29,21 @@ const constructBilling = (
   studentId: student.id
 })
 
-// We have handle the logic from billing to a student, calculate pending semester or months and return this
 export default async function billingHandle(req: NextApiRequest, res: NextApiResponse) {
   const {
     method,
     query: { id: docNum }
   } = req
-
+  // Validamos que el método utilizado sea POST.
   if (method !== 'POST') {
     res.setHeader('Allow', ['POST'])
     res.status(405).end(`Method ${method} Not Allowed`)
   }
-
+  // Validamos que el docNum sea un string válido (entre 1 y 12 carácteres)
   const { success } = idValidation.safeParse(docNum)
   if (!success) return res.status(404).send(`Document Number ${docNum} Not Allowed`)
 
-  // buscamos a la persona, su estatus y última recibo generado.
-  //Obtenemos al estudiante y todos sus datos a partir de la cédula
+  //Obtenemos al estudiante a partir de su número de identificación (Document Number)
   const student = await prisma.student.findFirst({
     include: {
       person: {
@@ -64,10 +62,11 @@ export default async function billingHandle(req: NextApiRequest, res: NextApiRes
     },
     where: { person: { docNumber: Array.isArray(docNum) ? docNum[0] : docNum } }
   })
+  if (!student) return res.status(404).end(`Student not found`)
   // Obtenemos el semestre actual
   const semester = await prisma.semester.findFirst({ orderBy: { endDate: 'desc' } })
-
-  //Hacemos una query y contamos si existen
+  if (!semester) return res.status(404).end(`Current Semester not found`)
+  // Contamos si existen cobros efectuados al semestre actual
   const billingCount = await prisma.billing.count({
     where: {
       student: { id: { equals: student?.id } },
@@ -75,27 +74,30 @@ export default async function billingHandle(req: NextApiRequest, res: NextApiRes
     }
   })
 
-  console.log({ billingCount })
-  
-  // Generamos los productos correspondiente al semestre
+  // Generamos los productos correspondiente al semestre (Todas las mensualidades para el semestre actual)
   const createBilling = async () => {
+    const data: Prisma.Enumerable<Prisma.BillingCreateManyInput> = []
     const monthsOfSemester = semester ? calculateMonths(semester) : 0
+    // Obtenemos el producto 'mensualidad' de la base de datos
     const monthlyPayment = await prisma.product.findFirst({
       where: { name: { contains: 'Mensualidad', mode: 'insensitive' } }
     })
+    if (!monthlyPayment) return res.status(404).end(`Monthly Payment product not found`)
+    // Obtenemos el producto 'Inscripción' de la base de datos
     const inscription = await prisma.product.findFirst({
       where: { name: { contains: 'Inscripción', mode: 'insensitive' } }
     })
-    const data: Prisma.Enumerable<Prisma.BillingCreateManyInput> = []
+    if (!inscription) return res.status(404).end(`Inscription product not found`)
+    // Iteramos los meses del semestre y creamos los objetos pertinentes de 'cobro'
     for (let i = 0; i <= monthsOfSemester; i++) {
       const newMonth = dayjs(semester?.startDate).locale('es').add(i, 'month').format('MMMM')
       const productName = `Mensualidad de ${newMonth}`
 
-      if (monthlyPayment && semester)
+      if (monthlyPayment)
         data.push(constructBilling(monthlyPayment, student as Student, semester, productName))
     }
-    //Tomamos la cantidad de meses, validamos si son 6 o más.
-    if (monthsOfSemester <= 5 && semester && inscription)
+    // si el semestre tiene menos de 6 meses, entonces creamos una inscripción por cobrar
+    if (monthsOfSemester < 6)
       data.push(
         constructBilling(
           inscription,
@@ -108,25 +110,8 @@ export default async function billingHandle(req: NextApiRequest, res: NextApiRes
     return await prisma.billing.createMany({ data })
   }
 
-
-  // tomar el mes de inicio del semestre y restarlo al mes final del semestre.
-  // Contar desde el mes de inicio hasta el final incluyendo estos dos.
-  // ¿Se debería añadir un campo booleano al semestre para indicar que está activo?
-  // EJEMPLO: Semestre 2022-II inicio 5 Septiembre 2022 -> Finaliza el 6 de Enero de 2023 ... Septiembre, Octubre, Noviembre, Diciembre, Enero... + Inscripción (mes anterior al inicio de sem)
-  // EJEMPLO2: Semestre 2023-II inicio 4 de febrero 2023 -> Finaliza el 9 de Agosto de 2023 ... Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto... sin Inscripción ? (hay 7 meses)
-  // Sí se tienen menos de 5 meses, se adicionará la inscripción, en caso contrario (hay 6 o más meses) solo se cobrarán las mensualidades.
-
-  // NO hago nada si el estudiante es distinto a "Matriculado" id: 1
-  // Si reviso la lista de cobros del estudiante:
-  // Tiene cobros en false o NO tiene
-  // Y estos NO son del semestre actual
-  // Se generan los cobros del semestre actual
-  // const existThisSemester = billings.some((billing) => billing.semester.id === semester?.id)
-
   if (billingCount === 0) await createBilling()
-  // No existen cobros de este semestre. Generamos los cobros pertinentes
 
-  // Obtenemos el arreglo de las mensualidades por cobrar...
   const billings = await prisma.billing.findMany({
     select: {
       id: true,
@@ -142,10 +127,26 @@ export default async function billingHandle(req: NextApiRequest, res: NextApiRes
       isCharged: { equals: false }
     }
   })
-  // *no puede pagar semestre actual si no ha pagado anteriores*
-  // Si reviso la lista de cobros, tiene cobros y estos son del semestre actual, no realizo ninguna creación y devuelvo todos los que estén en false
-
-  // Tenemos cobros por realizar. Puede que sean del semestre pasado.
 
   res.status(200).json({ student, billings })
 }
+
+// We have handle the logic from billing to a student, calculate pending semester or months and return this
+//Tomamos la cantidad de meses, validamos si son 6 o más.
+// tomar el mes de inicio del semestre y restarlo al mes final del semestre.
+// Contar desde el mes de inicio hasta el final incluyendo estos dos.
+// ¿Se debería añadir un campo booleano al semestre para indicar que está activo?
+// EJEMPLO: Semestre 2022-II inicio 5 Septiembre 2022 -> Finaliza el 6 de Enero de 2023 ... Septiembre, Octubre, Noviembre, Diciembre, Enero... + Inscripción (mes anterior al inicio de sem)
+// EJEMPLO2: Semestre 2023-II inicio 4 de febrero 2023 -> Finaliza el 9 de Agosto de 2023 ... Febrero, Marzo, Abril, Mayo, Junio, Julio, Agosto... sin Inscripción ? (hay 7 meses)
+// Sí se tienen menos de 5 meses, se adicionará la inscripción, en caso contrario (hay 6 o más meses) solo se cobrarán las mensualidades.
+// NO hago nada si el estudiante es distinto a "Matriculado" id: 1
+// Si reviso la lista de cobros del estudiante:
+// Tiene cobros en false o NO tiene
+// Y estos NO son del semestre actual
+// Se generan los cobros del semestre actual
+// const existThisSemester = billings.some((billing) => billing.semester.id === semester?.id)
+// No existen cobros de este semestre. Generamos los cobros pertinentes
+// Obtenemos el arreglo de las mensualidades por cobrar...
+// *no puede pagar semestre actual si no ha pagado anteriores*
+// Si reviso la lista de cobros, tiene cobros y estos son del semestre actual, no realizo ninguna creación y devuelvo todos los que estén en false
+// Tenemos cobros por realizar. Puede que sean del semestre pasado.
