7dd1258501f54bd3ba300dde111363dcf1cd76a5
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-12 16:10:04 -0400

    feat: show role description

--- components/settings/roles/RoleItem.tsx
+++ components/settings/roles/RoleItem.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading } from '@chakra-ui/react'
+import { Heading, Text } from '@chakra-ui/react'
 import { Role } from '@prisma/client'
 import { SimpleBox } from 'components/app'
 import { EditRoleModal } from './EditRoleModal'
@@ -9,10 +9,6 @@ export const RoleItem = ({ role }: { role: Role }) => (
     <Heading as="h2" size="sm" mb={2}>
       {role.name}
     </Heading>
-    <Box as="ul" listStylePos="inside">
-      <li>Puede hacer de todo</li>
-      <li>Y un par de cosas más</li>
-      <li>Como X, Y y Z</li>
-    </Box>
+    <Text>{role.description}</Text>
   </SimpleBox>
 )
