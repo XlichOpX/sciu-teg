b718ed73171ea24ced4489fb18fb09d305897803
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-12 11:56:42 -0400

    build: organize reports imports

--- components/reports/Sidebar.tsx
+++ components/reports/Sidebar.tsx
@@ -2,61 +2,57 @@ import { Button, Checkbox, FormControl, FormLabel, Input, Select, VStack } from 
 import { Select as RSelect } from 'chakra-react-select'
 import { SimpleBox } from 'components/app'
 
-function Sidebar() {
-  return (
-    <VStack align="stretch" as="form">
-      <SimpleBox>
-        <VStack>
-          <FormControl>
-            <FormLabel>Tipo de informe</FormLabel>
-            <Select>
-              <option>Arqueo de caja</option>
-            </Select>
-          </FormControl>
+export const Sidebar = () => (
+  <VStack align="stretch" as="form">
+    <SimpleBox>
+      <VStack>
+        <FormControl>
+          <FormLabel>Tipo de informe</FormLabel>
+          <Select>
+            <option>Arqueo de caja</option>
+          </Select>
+        </FormControl>
 
-          <FormControl>
-            <FormLabel>Fecha de inicio</FormLabel>
-            <Input type="date" />
-          </FormControl>
+        <FormControl>
+          <FormLabel>Fecha de inicio</FormLabel>
+          <Input type="date" />
+        </FormControl>
 
-          <FormControl>
-            <FormLabel>Fecha de fin</FormLabel>
-            <Input type="date" />
-          </FormControl>
+        <FormControl>
+          <FormLabel>Fecha de fin</FormLabel>
+          <Input type="date" />
+        </FormControl>
 
-          <Button w="full" colorScheme="blue">
-            Ver informe
-          </Button>
+        <Button w="full" colorScheme="blue">
+          Ver informe
+        </Button>
+      </VStack>
+    </SimpleBox>
+
+    <SimpleBox>
+      <FormControl>
+        <FormLabel>Métodos de pago</FormLabel>
+        <VStack align="flex-start">
+          <Checkbox>Efectivo</Checkbox>
+          <Checkbox>Tarjeta</Checkbox>
+          <Checkbox>Transferencia</Checkbox>
         </VStack>
-      </SimpleBox>
+      </FormControl>
+    </SimpleBox>
 
-      <SimpleBox>
-        <FormControl>
-          <FormLabel>Métodos de pago</FormLabel>
-          <VStack align="flex-start">
-            <Checkbox>Efectivo</Checkbox>
-            <Checkbox>Tarjeta</Checkbox>
-            <Checkbox>Transferencia</Checkbox>
-          </VStack>
-        </FormControl>
-      </SimpleBox>
-
-      <SimpleBox>
-        <FormControl>
-          <FormLabel>Cuentas</FormLabel>
-          <RSelect
-            placeholder="Seleccionar cuentas"
-            isMulti
-            options={[
-              { value: 'X', label: 'Cuenta X' },
-              { value: 'Y', label: 'Cuenta Y' },
-              { value: 'Z', label: 'Cuenta Z' }
-            ]}
-          />
-        </FormControl>
-      </SimpleBox>
-    </VStack>
-  )
-}
-
-export default Sidebar
+    <SimpleBox>
+      <FormControl>
+        <FormLabel>Cuentas</FormLabel>
+        <RSelect
+          placeholder="Seleccionar cuentas"
+          isMulti
+          options={[
+            { value: 'X', label: 'Cuenta X' },
+            { value: 'Y', label: 'Cuenta Y' },
+            { value: 'Z', label: 'Cuenta Z' }
+          ]}
+        />
+      </FormControl>
+    </SimpleBox>
+  </VStack>
+)
--- components/reports/index.ts
+++ components/reports/index.ts
@@ -1,1 +1,3 @@
-export { default as Sidebar } from './Sidebar'
+// created from 'create-ts-index'
+
+export * from './Sidebar';
