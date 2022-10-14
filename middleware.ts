06e0ca873f27b69416154b40626f9434f267f150
kurokuro15 <kurokuro15000@gmail.com>
2022-10-14 11:19:15 -0400

    Add logic for api authentication

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,26 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 import { getIronSession } from 'iron-session/edge'
 import { ironOptions } from 'lib/ironSession'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 export const middleware = async (req: NextRequest) => {
   const res = NextResponse.next()
   const session = await getIronSession(req, res, ironOptions)
-
+  const { pathname } = req.nextUrl
   const { user } = session
 
-  if (!user) {
-    return NextResponse.redirect(new URL(`/auth/login?redirect=${req.nextUrl.pathname}`, req.url))
+  const isApiRoute = pathname.includes('/api')
+  const isAuth = pathname.includes('/auth/login')
+  const isUnauthorized = pathname.includes('/unauthorized')
+
+  if (!isAuth && !user && !isUnauthorized) {
+    if (isApiRoute) return NextResponse.redirect(new URL('/api/unauthorized', req.url))
+    else return NextResponse.redirect(new URL(`/auth/login?redirect=${pathname}`, req.url))
   }
 
   return res
 }
 
 export const config = {
-  matcher: ['/access']
+  matcher: ['/access', '/api/:path*']
 }
