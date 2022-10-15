0eead927a00d04775b4cd06559c60c692787624c
kurokuro15 <kurokuro15000@gmail.com>
2022-10-15 18:31:04 -0400

    Add pagination, search by after date and validation permission of user

--- pages/api/conversion/index.ts
+++ pages/api/conversion/index.ts
@@ -1,25 +1,53 @@
-import { Conversion } from '@prisma/client'
-import prisma from '../../../lib/prisma'
+import { Conversion, Prisma } from '@prisma/client'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { ironOptions } from 'lib/ironSession'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { canUnserDo } from 'utils/checkPermissions'
+import { routePaginate, search } from 'utils/routePaginate'
+import prisma from '../../../lib/prisma'
 
 // GET|POST /api/conversion
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { body, method } = req
+export default withIronSessionApiRoute(handle, ironOptions)
 
+async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const { body, method, session, query } = req
+  if (!canUnserDo(session, 'READ_CONVERSIONS')) return res.status(403).send(`Can't read this.`)
   switch (method) {
     case 'GET':
-      //obtenemos TODAS las conversiones
-      const conversions: Conversion[] | null = await prisma.conversion.findMany()
+      try {
+        const { after } = query
 
-      if (!conversions) return res.status(404).end(`Conversions not found`)
-      res.status(200).send(conversions)
+        const where = { date: search<Prisma.DateTimeFilter>(after) }
+
+        //obtenemos TODAS las conversiones
+        const result = await prisma.conversion.findMany({
+          ...routePaginate(query),
+          where
+        })
+
+        const count = await prisma.conversion.count({ where })
+
+        res.status(200).send({ count, result })
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).send(error.message)
+        }
+      }
       break
+
     case 'POST':
-      //creamos UNA conversión
-      const result: Conversion = await prisma.conversion.create({
-        data: { ...body }
-      })
-      res.status(201).send(result)
+      if (!canUnserDo(session, 'EDIT_CONVERSIONS')) return res.status(403).send(`Can't edit this.`)
+      try {
+        //creamos UNA conversión
+        const result: Conversion = await prisma.conversion.create({
+          data: { ...body }
+        })
+        res.status(201).send(result)
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).send(error.message)
+        }
+      }
       break
     default:
       res.setHeader('Allow', ['GET', 'POST'])
