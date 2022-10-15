1f412576a3998a951e00d41f0161f82c836cf8fc
kurokuro15 <kurokuro15000@gmail.com>
2022-10-15 15:15:18 -0400

    Add pagination, search by keyword and permission validation in Receipt EP

--- pages/api/receipt/index.ts
+++ pages/api/receipt/index.ts
@@ -1,25 +1,69 @@
-import { Receipt } from '@prisma/client'
-import prisma from '../../../lib/prisma'
+import { Prisma } from '@prisma/client'
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { ironOptions } from 'lib/ironSession'
+import prisma from 'lib/prisma'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { canUnserDo } from 'utils/checkPermissions'
+import { routePaginate, search } from 'utils/routePaginate'
 
 // GET|POST /api/receipt
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-  const { body, method } = req
+export default withIronSessionApiRoute(handle, ironOptions)
+
+async function handle(req: NextApiRequest, res: NextApiResponse) {
+  const { body, method, query, session } = req
 
   switch (method) {
     case 'GET':
-      //obtenemos TODOS los recibos
-      const receipts: Receipt[] | null = await prisma.receipt.findMany()
+      if (!canUnserDo(session, 'READ_RECEIPT')) return res.status(403).send(`Can't read this.`)
+      // destructuring limit and offset values from query params
+      const { keyword } = query
+      const searchQuery = search(keyword)
+      // obtenemos TODOS los productos
 
-      if (!receipts) return res.status(404).end(`Reecipts not found`)
-      res.status(200).send(receipts)
+      const where: Prisma.ReceiptWhereInput = {
+        OR: [
+          { person: { firstName: searchQuery } },
+          { person: { middleName: searchQuery } },
+          { person: { secondLastName: searchQuery } },
+          { person: { firstLastName: searchQuery } }
+        ]
+      }
+
+      const count = await prisma.receipt.count({ where })
+      //obtenemos TODOS los recibos
+      const result = await prisma.receipt.findMany({
+        select: {
+          amount: true,
+          createdAt: true,
+          id: true,
+          person: {
+            select: {
+              docType: { select: { type: true } },
+              docNumber: true,
+              firstName: true,
+              firstLastName: true
+            }
+          }
+        },
+        ...routePaginate(query),
+        where
+      })
+
+      res.json({ count, result })
       break
     case 'POST':
+      if (!canUnserDo(session, 'CREATE_RECEIPT')) return res.status(403).send(`Can't create this.`)
       //creamos UN recibo
-      const result: Receipt = await prisma.receipt.create({
-        data: { ...body }
-      })
-      res.status(201).send(result)
+      try {
+        const result = await prisma.receipt.create({
+          data: { ...body }
+        })
+        res.status(201).send(result)
+      } catch (error) {
+        if (error instanceof Error) {
+          res.status(400).send(error.message)
+        }
+      }
       break
     default:
       res.setHeader('Allow', ['GET', 'POST'])
