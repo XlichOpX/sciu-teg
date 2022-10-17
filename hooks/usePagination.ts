f02c0fd3ed922703d61d550d750cd74ab18a6a61
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-17 10:39:16 -0400

    feat: save current page in querystring

--- hooks/usePagination.ts
+++ hooks/usePagination.ts
@@ -1,4 +1,11 @@
-import { useLayoutEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useLayoutEffect } from 'react'
+import { z } from 'zod'
+
+const pageValidator = z.preprocess(
+  (arg) => parseInt(arg as string, 10),
+  z.number().int().positive().default(1)
+)
 
 export const usePagination = ({
   initialPage = 1,
@@ -9,18 +16,21 @@ export const usePagination = ({
   itemsPerPage?: number
   scrollToTop?: boolean
 } = {}) => {
-  const [page, setPageState] = useState(initialPage)
+  const router = useRouter()
+  const validationResult = pageValidator.safeParse(router.query.page)
+  const page = validationResult.success ? validationResult.data : initialPage
 
   useLayoutEffect(() => {
     scrollToTop && window.scroll({ top: 0 })
-  }, [page, scrollToTop])
+  }, [scrollToTop])
 
   return {
     offset: (page - 1) * itemsPerPage,
     limit: itemsPerPage,
     page,
     setPage: (page: number) => {
-      setPageState(page)
+      router.query.page = page.toString()
+      router.push(router)
     }
   }
 }
