698e446dd5b80fa1e9df4c5bbdff75c18675f9e9
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-20 19:28:35 -0400

    fix: try to fix receipt error

--- hooks/useReceipt.ts
+++ hooks/useReceipt.ts
@@ -1,6 +1,8 @@
 import useSWR from 'swr'
 
-export const useReceipt = (receiptId: number) => {
-  const { data, error } = useSWR<Record<string, any>, Error>(`/api/receipt/${receiptId}`)
+export const useReceipt = (receiptId?: number) => {
+  const { data, error } = useSWR<Record<string, any>, Error>(
+    receiptId ? `/api/receipt/${receiptId}` : null
+  )
   return { receipt: data, error }
 }
