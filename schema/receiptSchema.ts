4dd9da2ab7cf2387a661d9e3454b4a711e8b34c1
kurokuro15 <kurokuro15000@gmail.com>
2022-10-22 20:01:38 -0400

    Add ReceiptSchema

---
+++ schema/receiptSchema.ts
@@ -0,0 +1,44 @@
+import { z } from 'zod'
+
+// Schema from validate create Receipt.
+/**
+  body:{
+    amount: number,
+    billings: number[] | undefined,
+    charges: {
+      amount: number
+      paymentMethod: {
+        id: number,
+        metaPayment: { [x:string]: string } | undefined,
+        conversion: number
+      }
+    }[],
+    products: {
+      id: number,
+      quantity: number
+    } | undefined,
+    person: number
+  }
+ **/
+export const receiptCreateSchemaInput = z.object({
+  amount: z.number().positive(),
+  billings: z.number().positive().int().array().nullish(),
+  charges: z
+    .object({
+      amount: z.number().positive(),
+      paymentMethod: z.object({
+        id: z.number().positive(),
+        metaPayment: z.record(z.string()).nullish(),
+        conversion: z.number().positive()
+      })
+    })
+    .array(),
+  products: z
+    .object({
+      id: z.number().positive().int(),
+      quantity: z.number().positive().int()
+    })
+    .array()
+    .nullish(),
+  person: z.number().positive().int()
+})
