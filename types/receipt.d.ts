3b9e0aab74ef5aedc446714f3cf177738690e881
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-24 16:08:26 -0400

    build: add CreateReceiptInput type

--- types/receipt.d.ts
+++ types/receipt.d.ts
@@ -1,6 +1,9 @@
 import { Prisma } from '@prisma/client'
 import { receiptWithAll, receiptWithPerson } from 'prisma/queries'
+import { receiptCreateSchemaInput } from 'schema/receiptSchema'
+import { z } from 'zod'
 
 export type ReceiptWithPerson = Prisma.ReceiptGetPayload<typeof receiptWithPerson>
 export type GetReceiptWithPersonResponse = { count: number; result: ReceiptWithPerson[] }
 export type ReceiptWithAll = Prisma.ReceiptGetPayload<typeof receiptWithAll>
+export type CreateReceiptInput = z.infer<typeof receiptCreateSchemaInput>
