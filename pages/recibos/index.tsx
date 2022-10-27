198955575537e0ae60e3d93dfee708d1f79502f7
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-10-27 17:03:13 -0400

    feat: add spinner to '/recibos'

--- pages/recibos/index.tsx
+++ pages/recibos/index.tsx
@@ -1,5 +1,5 @@
 import { Alert } from '@chakra-ui/react'
-import { HeadingWithSearch, Pagination } from 'components/app'
+import { FullyCenteredSpinner, HeadingWithSearch, Pagination } from 'components/app'
 import { BaseLayout } from 'components/layouts'
 import { ReceiptList } from 'components/receipts'
 import { useReceipts } from 'hooks'
@@ -7,7 +7,7 @@ import Head from 'next/head'
 import { NextPageWithLayout } from '../_app'
 
 const Receipts: NextPageWithLayout = () => {
-  const { receipts, pages, page, setPage, setSearch } = useReceipts({ itemsPerPage: 20 })
+  const { receipts, pages, page, setPage, setSearch, isLoading } = useReceipts({ itemsPerPage: 20 })
   return (
     <>
       <Head>
@@ -19,7 +19,7 @@ const Receipts: NextPageWithLayout = () => {
         placeholder="Buscar recibos"
         onChange={({ text }) => setSearch(text)}
       />
-
+      {isLoading && <FullyCenteredSpinner />}
       {receipts && receipts.length > 0 && <ReceiptList receipts={receipts} />}
       {receipts && receipts.length === 0 && <Alert>No hay resultados...</Alert>}
       {!!pages && pages > 0 && <Pagination page={page} pages={pages} setPage={setPage} />}
