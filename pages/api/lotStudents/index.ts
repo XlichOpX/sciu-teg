43808b2af4816531b09f9d3f0a53311339b93506
kurokuro15 <kurokuro15000@gmail.com>
2022-11-05 17:30:38 -0400

    FIx: Resolving some error of validation and check date

--- pages/api/lotStudents/index.ts
+++ pages/api/lotStudents/index.ts
@@ -1,14 +1,19 @@
+import { Receipt } from '@prisma/client'
 import { withIronSessionApiRoute } from 'iron-session/next'
+import dayjs from 'lib/dayjs'
 import { ironOptions } from 'lib/ironSession'
+import prisma from 'lib/prisma'
+import _ from 'lodash'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import type { MetaPaymentData, MetaPaymentDef } from 'types/paymentMethod'
+import { CreateReceiptInput } from 'types/receipt'
+import { insertReceipt } from 'utils/insertReceipt'
+import { stringSearch } from 'utils/routePaginate'
 import XLSX from 'xlsx'
 
-// Disable `bodyParser` to consume as stream
-// export const config = {
-//   api: {
-//     bodyParser: false
-//   }
-// }
+const SUNDAY = 0
+const FRIDAY = 5
+const SATURDAY = 6
 
 export default withIronSessionApiRoute(handle, ironOptions)
 
@@ -17,24 +22,104 @@ async function handle(req: NextApiRequest, res: NextApiResponse) {
     try {
       // obtenemos los datos en un formate manejable.
       const { dataCell } = handleBody(req.body)
-      const charges = parseData(dataCell)
+      const byDocNumber = parseData(dataCell)
+      const receiptArr: Receipt[] = []
+      for (const docNum in byDocNumber) {
+        const person = await prisma.student.findFirstOrThrow({
+          select: { personId: true },
+          where: { person: { docNumber: { equals: docNum } } }
+        })
 
+        const rows = byDocNumber[docNum] //tomamos el array de filas referentes al estudiante.
+        const billings: CreateReceiptInput['billings'] = []
+        const products: CreateReceiptInput['products'] = []
+        const charges: CreateReceiptInput['charges'] = []
+        let receiptAmount = 0
+
+        for (const row of rows) {
+          const billing = await prisma.billing.findFirst({
+            select: { id: true, amount: true, isCharged: true },
+            where: { productName: stringSearch(row.productName), isCharged: false }
+          })
+          const product = await prisma.product.findFirst({
+            select: { id: true, price: true },
+            where: { name: stringSearch(row.productName) }
+          })
+          let amount: number
+          if (billing) {
+            billings.push(billing.id)
+            amount = billing.amount
+          } else if (product) {
+            products.push({ id: product.id, quantity: row.quantity })
+            amount = product.price
+          } else {
+            throw new Error('Billing or Product not Found')
+          }
+          //Cobros
+          const paymentMethod = await prisma.paymentMethod.findFirstOrThrow({
+            select: { id: true, metaPayment: true },
+            where: { name: stringSearch(row.paymentMethod) }
+          })
+          const metaPayment = (paymentMethod.metaPayment as MetaPaymentDef[])?.map(
+            (metaPayment: MetaPaymentDef) => {
+              return {
+                name: metaPayment.name,
+                fieldType: metaPayment.fieldType,
+                value: row[metaPayment.name as keyof typeof row] // viene de la row
+              } as MetaPaymentData
+            }
+          )
+          let date = dayjs(row.fecha)
+          const day = date.day()
+          if (day === SATURDAY || day === SUNDAY) date = date.subtract(day - FRIDAY, 'day')
+          const gt = date.set('h', 0).set('m', 0).set('s', 0).set('ms', 0).toDate()
+          const lt = date.set('h', 23).set('m', 59).set('s', 59).set('ms', 999).toDate()
+          const conversion = await prisma.conversion.findFirstOrThrow({
+            select: { id: true, dolar: true },
+            where: { date: { lt, gt } },
+            orderBy: { date: 'desc' }
+          })
+          //Convertimos en bolívares el valor del producto
+          const amountConv = amount * conversion.dolar
+          if (amountConv > row.amount)
+            throw new Error(` the value: ${amountConv} is greater than ${row.amount} validate.`)
+          // Acumulamos el monto del cobro para el recibo
+          receiptAmount += amount
+          charges.push({
+            paymentMethod: { metaPayment, id: paymentMethod.id, conversion: conversion.id },
+            amount
+          })
+        }
+
+        // if (charges.length < 0 || !(billings.length < 0) || products.length < 0)
+        //   throw new Error(`Charges, billing or products it's empty`)
+
+        const receiptData: CreateReceiptInput = {
+          billings,
+          charges,
+          products,
+          person: person.personId,
+          amount: receiptAmount
+        }
+        // console.log(JSON.stringify({ receiptData }, null, 2))
+        const receipt = await insertReceipt(receiptData)
+        console.log({ receipt })
+        receiptArr.push(receipt)
+      }
+      // const result = {}
       // Preparamos las queries a realizar
-      res.json({ response: dataCell, sortCharges: charges })
+      res.json({ receiptArr })
     } catch (error) {
       console.error(error)
-      res.send(JSON.stringify({ error: error }, null, 2))
+      res.send({ error })
     }
   } else {
     res.setHeader('Allow', ['POST'])
     res.status(405).end(`Method ${req.method} Not Allowed`)
   }
 }
-
 /**
  * Enpoint temporal para la creación de la lógica de cargar un excel con muchos cobros.
- *
- *
  * Para lograr esto requerimos dos partes de lógica.
  * En el front, surtir una interface con un uploader de archivos EXCEL
  * (SOLO 'Hojas de Cálculo' o CSV, aún por definir las extensiones a admitir, falta buscar una lib que lo haga)
@@ -63,6 +148,7 @@ async function handle(req: NextApiRequest, res: NextApiResponse) {
  * En caso de error NADA debería de hacerse. (todo el proceso debería de invertirse y no tocar nada.)
  *
  */
+
 /**
  * manejo del cuerpo de la request para convertir el archivo en un objeto manejable
  * con XLSX
@@ -104,26 +190,24 @@ const parseData = (
 ) => {
   if (!dataCell) throw Error(`not dataCell`)
   const rows = dataCell.map((row) => {
-    const person = row['cedula']
+    const person = row['cedula'] as string
     if (!person) throw new Error(`not found data in ${row['cedula_addr']}`)
-    const productPrice = row['precio']
+    const productPrice = row['precio'] as number
     if (!productPrice) throw new Error(`not found data in ${row['precio_addr']}`)
-    const quantity = row['cantidad']
+    const quantity = row['cantidad'] as number
     if (!quantity) throw new Error(`not found data in ${row['cantidad_addr']}`)
-    const paymentMethod = row['metodo_de_pago']
+    const paymentMethod = row['metodo_de_pago'] as string
     if (!paymentMethod) throw new Error(`not found data in ${row['metodo_de_pago_addr']}`)
-    const amount = row['monto_cobrado']
+    const amount = row['monto_cobrado'] as number
     if (!amount) throw new Error(`not found data in ${row['monto_cobrado_addr']}`)
-    const paymentMethodRef = row['referencia']
-    if (!paymentMethodRef) throw new Error(`not found data in ${row['referencia_addr']}`)
-    const paymentMethodDate = new Date(row['fecha'])
-    if (!paymentMethodDate) throw new Error(`not found data in ${row['fecha_addr']}`)
-    const conversion = row['conversion']
-    if (!conversion) throw new Error(`not found data in ${row['conversion_addr']}`)
+    const referencia = row['referencia'] as string
+    if (!referencia) throw new Error(`not found data in ${row['referencia_addr']}`)
+    const fecha = new Date(row['fecha'] as unknown as Date)
+    if (!fecha) throw new Error(`not found data in ${row['fecha_addr']}`)
     const productName =
       row['producto'] == 'mensualidad'
         ? `${row['mensualidad']} ${row['semestre']}`
-        : row['producto']
+        : (row['producto'] as string)
     if (!productName)
       throw new Error(
         `not fount data in ${
@@ -138,34 +222,10 @@ const parseData = (
       quantity,
       paymentMethod,
       amount,
-      paymentMethodRef,
-      paymentMethodDate,
-      conversion,
+      referencia,
+      fecha,
       productName
     }
   })
-  return rows
+  return _.groupBy(rows, 'person')
 }
-/**
-body: {
-  amount: number      === SUMA(productos|billing.amount) 
-  billings: number    Where billing.productName === producto
-  charges: {
-    amount: number    === monto_cobrado/SUMA(productos|billing.amount)
-    paymentMethod: {  where paymentMethod.name === metodo_de_pago
-      id: number
-      metaPayment:{
-        name:         string          ===   referencia  &&  fecha
-        value:        string          ===   ref_de_pago &&  fecha_de_pago
-        fieldType:    string | date   ===   string      &&  date
-      }
-      conversion: number  where monto_cobrado/SUMA(productos|billing.amount) === conversion
-    }
-  }
-  products: {
-    id: number      Where product.name  === producto
-    quantity: number                    === cantidad
-  }
-  person: number    Where DocNum        === cedula_estudiante
-}
- **/
