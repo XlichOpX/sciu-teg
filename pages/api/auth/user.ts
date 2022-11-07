49334cb1d7daa8a2217d1156fd67ade319765729
kurokuro15 <kurokuro15000@gmail.com>
2022-11-07 16:33:07 -0400

    Feat: Adding user route for retrieve all data in Cookie

---
+++ pages/api/auth/user.ts
@@ -0,0 +1,24 @@
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { ironOptions } from 'lib/ironSession'
+import prisma from 'lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+export default withIronSessionApiRoute(handle, ironOptions)
+
+async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { session } = req
+    const { user } = session
+    if (!user) return res.status(400).send(`cookie invalid.`)
+    const roles = user?.role?.map((rol) => rol.id)
+    const permissions = await prisma.permission.findMany({
+      where: { roles: { some: { id: { in: roles } } } }
+    })
+    const response = { ...user, permissions }
+
+    res.status(200).json(response)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(400).send(error.message)
+    }
+  }
+}
