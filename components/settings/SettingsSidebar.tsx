7fef97226af5b200aa17fed2958f291dcc1c5250
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-08 09:34:08 -0400

    feat: hide or show SettingsSidebar links based on user perms

--- components/settings/SettingsSidebar.tsx
+++ components/settings/SettingsSidebar.tsx
@@ -1,4 +1,6 @@
 import { Button, Divider, Flex } from '@chakra-ui/react'
+import { FullyCenteredSpinner } from 'components/app'
+import { useAuth } from 'hooks'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { BsCreditCardFill, BsGearFill, BsPersonFill } from 'react-icons/bs'
@@ -9,47 +11,56 @@ const links = [
   {
     href: '/configuracion/general',
     text: 'General',
-    icon: <BsGearFill />
+    icon: <BsGearFill />,
+    permission: 'READ_PARAMETER'
   },
   {
     href: '/configuracion/usuarios',
     text: 'Usuarios',
-    icon: <BsPersonFill />
+    icon: <BsPersonFill />,
+    permission: 'READ_USER'
   },
   {
     href: '/configuracion/roles',
     text: 'Roles',
-    icon: <FaRedhat />
+    icon: <FaRedhat />,
+    permission: 'READ_ROLE'
   },
   {
     href: '/configuracion/metodos-de-pago',
     text: 'Métodos de pago',
-    icon: <BsCreditCardFill />
+    icon: <BsCreditCardFill />,
+    permission: 'READ_PAYMENTMETHOD'
   },
   {
     href: '/configuracion/tasas-de-cambio',
     text: 'Tasas de cambio',
-    icon: <FaExchangeAlt />
+    icon: <FaExchangeAlt />,
+    permission: 'READ_CONVERSION'
   },
   {
     href: '/configuracion/categorias',
     text: 'Categorías',
-    icon: <MdCategory />
+    icon: <MdCategory />,
+    permission: 'READ_CATEGORY'
   },
   {
     href: '/configuracion/monedas',
     text: 'Monedas',
-    icon: <FaCoins />
+    icon: <FaCoins />,
+    permission: 'READ_CURRENCY'
   },
   {
     href: '/configuracion/tipos-de-documento',
     text: 'Tipos de documento',
-    icon: <FaIdCardAlt />
+    icon: <FaIdCardAlt />,
+    permission: 'READ_DOCTYPE'
   },
   {
     href: '/configuracion/carreras',
     text: 'Carreras',
-    icon: <FaGraduationCap />
+    icon: <FaGraduationCap />,
+    permission: 'READ_CAREER'
   }
 ]
 
@@ -61,25 +72,33 @@ links.sort((a, b) => {
 
 export const SettingsSidebar = () => {
   const { asPath } = useRouter()
+
+  const { user } = useAuth()
+  if (!user) return <FullyCenteredSpinner />
+
   return (
     <>
       <Flex as="ul" direction="column" gap={4} listStyleType="none">
-        {links.map((li) => (
-          <li key={li.href}>
-            <Link href={li.href} passHref>
-              <Button
-                as="a"
-                w="full"
-                variant={asPath === li.href ? 'solid' : 'ghost'}
-                justifyContent="flex-start"
-                gap={4}
-                pointerEvents={asPath === li.href ? 'none' : undefined}
-              >
-                {li.icon} {li.text}
-              </Button>
-            </Link>
-          </li>
-        ))}
+        {links.map((li) => {
+          if (user.permissions.includes(li.permission)) {
+            return (
+              <li key={li.href}>
+                <Link href={li.href} passHref>
+                  <Button
+                    as="a"
+                    w="full"
+                    variant={asPath === li.href ? 'solid' : 'ghost'}
+                    justifyContent="flex-start"
+                    gap={4}
+                    pointerEvents={asPath === li.href ? 'none' : undefined}
+                  >
+                    {li.icon} {li.text}
+                  </Button>
+                </Link>
+              </li>
+            )
+          }
+        })}
       </Flex>
       <Divider mt={4} display={['block', 'none']} />
     </>
