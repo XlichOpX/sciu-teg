c432522dd5b9aa05fb64b6bce9ddc1cc646e1dc1
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-08 09:54:55 -0400

    feat: show or hide Career UI based on user perms

--- components/settings/careers/CareerItem.tsx
+++ components/settings/careers/CareerItem.tsx
@@ -1,11 +1,16 @@
 import { Text } from '@chakra-ui/react'
 import { SimpleBox } from 'components/app'
+import { useAuth } from 'hooks'
 import { Career } from 'types/career'
 import { EditCareerModal } from './EditCareerModal'
 
-export const CareerItem = ({ career }: { career: Career }) => (
-  <SimpleBox as="li" display="flex" justifyContent="space-between">
-    <Text fontWeight="bold">{career.career}</Text>
-    <EditCareerModal career={career} />
-  </SimpleBox>
-)
+export const CareerItem = ({ career }: { career: Career }) => {
+  const { user } = useAuth()
+
+  return (
+    <SimpleBox as="li" display="flex" justifyContent="space-between">
+      <Text fontWeight="bold">{career.career}</Text>
+      {user?.permissions.includes('EDIT_CAREER') && <EditCareerModal career={career} />}
+    </SimpleBox>
+  )
+}
--- pages/configuracion/carreras.tsx
+++ pages/configuracion/carreras.tsx
@@ -2,17 +2,22 @@ import { Alert, Divider, Flex } from '@chakra-ui/react'
 import { FullyCenteredSpinner, SearchInput } from 'components/app'
 import { SettingsLayout } from 'components/settings'
 import { CareerList, CreateCareerModal } from 'components/settings/careers'
-import { useCareers } from 'hooks'
+import { useAuth, useCareers } from 'hooks'
 import { NextPageWithLayout } from 'pages/_app'
 
 const CareerSettings: NextPageWithLayout = () => {
   const { careers, error, isLoading, setSearch } = useCareers()
+  const { user } = useAuth()
+
+  if (user && !user.permissions.includes('READ_CAREER')) {
+    return <Alert status="error">No tiene permiso para acceder a esta interfaz</Alert>
+  }
 
   return (
     <>
       <Flex direction={['column', 'row']} align="stretch" justify="space-between" gap={4}>
         <SearchInput placeholder="Buscar carreras" onChange={({ text }) => setSearch(text)} />
-        <CreateCareerModal />
+        {user?.permissions.includes('CREATE_CAREER') && <CreateCareerModal />}
       </Flex>
 
       <Divider my={4} />
