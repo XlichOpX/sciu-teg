d9ce7a660920330277e7099ac4920b740aef5dc5
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-08 12:08:39 -0400

    feat: show or hide Doctype UI based on user perms

--- components/settings/doctypes/DoctypeItem.tsx
+++ components/settings/doctypes/DoctypeItem.tsx
@@ -1,11 +1,16 @@
 import { Text } from '@chakra-ui/react'
 import { SimpleBox } from 'components/app'
+import { useAuth } from 'hooks'
 import { DocType } from 'types/doctype'
 import { EditDoctypeModal } from './EditDoctypeModal'
 
-export const DoctypeItem = ({ doctype }: { doctype: DocType }) => (
-  <SimpleBox as="li" display="flex" justifyContent="space-between">
-    <Text fontWeight="bold">{doctype.type}</Text>
-    <EditDoctypeModal doctype={doctype} />
-  </SimpleBox>
-)
+export const DoctypeItem = ({ doctype }: { doctype: DocType }) => {
+  const { user } = useAuth()
+
+  return (
+    <SimpleBox as="li" display="flex" justifyContent="space-between">
+      <Text fontWeight="bold">{doctype.type}</Text>
+      {user?.permissions.includes('EDIT_DOCTYPE') && <EditDoctypeModal doctype={doctype} />}
+    </SimpleBox>
+  )
+}
