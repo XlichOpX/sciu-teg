3eed915803e832e57e40ba8cf7e6ada7da97b059
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-08 10:35:50 -0400

    feat: adjust general settings UI based on user perms

--- pages/configuracion/general.tsx
+++ pages/configuracion/general.tsx
@@ -1,9 +1,16 @@
-import { Divider } from '@chakra-ui/react'
+import { Alert, Divider } from '@chakra-ui/react'
 import { SettingsLayout } from 'components/settings'
 import { ColorModeSwitch, InstituteDataForm } from 'components/settings/general'
+import { useAuth } from 'hooks'
 import { NextPageWithLayout } from 'pages/_app'
 
 const GeneralSettings: NextPageWithLayout = () => {
+  const { user } = useAuth()
+
+  if (!user?.permissions.includes('READ_PARAMETER')) {
+    return <Alert status="error">No tiene permiso para ver los parámetros generales</Alert>
+  }
+
   return (
     <>
       <InstituteDataForm />
