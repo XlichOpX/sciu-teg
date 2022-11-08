44f18b8068d8c38202907e338b1ddbfdb19efc84
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-08 11:34:33 -0400

    feat: hide or show Role UI based on user perms

--- components/settings/roles/RoleItem.tsx
+++ components/settings/roles/RoleItem.tsx
@@ -1,14 +1,19 @@
 import { Heading, Text } from '@chakra-ui/react'
 import { Role } from '@prisma/client'
 import { SimpleBox } from 'components/app'
+import { useAuth } from 'hooks'
 import { EditRoleModal } from './EditRoleModal'
 
-export const RoleItem = ({ role }: { role: Role }) => (
-  <SimpleBox as="li" pos="relative">
-    <EditRoleModal role={role} />
-    <Heading as="h2" size="sm" mb={2}>
-      {role.name}
-    </Heading>
-    <Text>{role.description}</Text>
-  </SimpleBox>
-)
+export const RoleItem = ({ role }: { role: Role }) => {
+  const { user } = useAuth()
+
+  return (
+    <SimpleBox as="li" pos="relative">
+      {user?.permissions.includes('EDIT_ROLE') && <EditRoleModal role={role} />}
+      <Heading as="h2" size="sm" mb={2}>
+        {role.name}
+      </Heading>
+      <Text>{role.description}</Text>
+    </SimpleBox>
+  )
+}
--- pages/configuracion/roles.tsx
+++ pages/configuracion/roles.tsx
@@ -2,17 +2,22 @@ import { Alert, Divider, Flex } from '@chakra-ui/react'
 import { FullyCenteredSpinner, SearchInput } from 'components/app'
 import { SettingsLayout } from 'components/settings'
 import { CreateRoleModal, RoleList } from 'components/settings/roles'
-import { useRoles } from 'hooks'
+import { useAuth, useRoles } from 'hooks'
 import { NextPageWithLayout } from 'pages/_app'
 
 const RoleSettings: NextPageWithLayout = () => {
-  const { roles, error, isLoading } = useRoles()
+  const { roles, error, isLoading, setSearch } = useRoles()
+
+  const { user } = useAuth()
+  if (user && !user.permissions.includes('READ_ROLE')) {
+    return <Alert status="error">No tiene permiso para ver los roles</Alert>
+  }
 
   return (
     <>
       <Flex direction={['column', 'row']} align="stretch" justify="space-between" gap={4}>
-        <SearchInput placeholder="Buscar roles" />
-        <CreateRoleModal />
+        <SearchInput placeholder="Buscar roles" onChange={({ text }) => setSearch(text)} />
+        {user?.permissions.includes('CREATE_ROLE') && <CreateRoleModal />}
       </Flex>
 
       <Divider my={4} />
