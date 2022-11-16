301456e88cc4a45c82527319c9ca0196b3ef451f
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-15 23:44:06 -0400

    fix: update type

--- types/currency.d.ts
+++ types/currency.d.ts
@@ -1,7 +1,7 @@
 import { Currency } from '@prisma/client'
-import { currencyCreateSchema } from 'schema/currencySchema'
+import { currencyCreateSchema, currencyUpdateSchema } from 'schema/currencySchema'
 import { z } from 'zod'
 
 export type Currency = Currency
 export type CreateCurrencyInput = z.infer<typeof currencyCreateSchema>
-export type UpdateCurrencyInput = CreateCurrencyInput
+export type UpdateCurrencyInput = z.infer<typeof currencyUpdateSchema>
