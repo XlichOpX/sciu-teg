874f294d1a484078ba108c10438fe06fca27b691
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-21 22:09:20 -0400

    feat: add print button and header to reports

--- pages/informes/index.tsx
+++ pages/informes/index.tsx
@@ -1,26 +1,34 @@
-import { Divider, Heading, Text } from '@chakra-ui/react'
+import { Alert, Box, Button, Divider, Heading, Text, VStack } from '@chakra-ui/react'
 import { BaseLayout, SidebarLayout } from 'components/layouts'
 import { Sidebar } from 'components/reports'
 import { ReportTypeKey, reportTypes } from 'components/reports/reportTypes'
 import Head from 'next/head'
 import { NextPageWithLayout } from 'pages/_app'
 import { useState } from 'react'
+import { BsPrinterFill } from 'react-icons/bs'
 import { getReport } from 'services/reports'
 import { Report } from 'types/report'
 import { hideOnPrint } from 'utils/hideOnPrint'
 
 const Reports: NextPageWithLayout = () => {
-  const [report, setReport] = useState<{ type: ReportTypeKey; data: Report }>()
+  const [report, setReport] = useState<{
+    type: ReportTypeKey
+    data: Report
+    start: string
+    end: string
+  }>()
   const ReportView = report ? reportTypes[report.type].component : undefined
+
+  const isOneDay = report?.start === report?.end
 
   return (
     <>
       <Box sx={{ ...hideOnPrint }}>
-      <Head>
-        <title>Informes</title>
-      </Head>
-      <Heading as="h1">Informes</Heading>
-      <Divider my={4} />
+        <Head>
+          <title>Informes</title>
+        </Head>
+        <Heading as="h1">Informes</Heading>
+        <Divider my={4} />
       </Box>
 
       <SidebarLayout
@@ -29,7 +37,13 @@ const Reports: NextPageWithLayout = () => {
             sx={{ ...hideOnPrint }}
             onSubmit={async (data) => {
               const report = await getReport(data)
-              setReport({ type: data.reportType, data: report })
+              const { reportType, start, end } = data
+              setReport({
+                type: reportType,
+                data: report,
+                start: start.replaceAll('-', '/'),
+                end: end.replaceAll('-', '/')
+              })
             }}
           />
         }
@@ -37,7 +51,31 @@ const Reports: NextPageWithLayout = () => {
       >
         {!report && <Text>Solicite un informe...</Text>}
         {report && ReportView ? (
-          <ReportView data={report.data} />
+          <>
+            <VStack mb={3}>
+              <Heading size="lg">{reportTypes[report.type].label}</Heading>
+              <span>{isOneDay ? report.start : `${report.start} - ${report.end}`}</span>
+              <Divider />
+            </VStack>
+
+            {report.data.length !== 0 ? (
+              <ReportView data={report.data} />
+            ) : (
+              <Alert>Sin movimientos...</Alert>
+            )}
+
+            <Button
+              mx="auto"
+              my={3}
+              display="block"
+              colorScheme="blue"
+              leftIcon={<BsPrinterFill />}
+              onClick={print}
+              sx={{ ...hideOnPrint }}
+            >
+              Imprimir
+            </Button>
+          </>
         ) : (
           <pre>{JSON.stringify(report, null, 2)}</pre>
         )}
