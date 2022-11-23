4cb4ee921b699e2077be0a23fae30fae51dd95f8
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-23 18:27:44 -0400

    fix: color mode bug

--- theme/index.ts
+++ theme/index.ts
@@ -1,8 +1,7 @@
 import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
 
 const config: ThemeConfig = {
-  initialColorMode: 'system',
-  useSystemColorMode: true
+  initialColorMode: 'system'
 }
 
 const Table = {
