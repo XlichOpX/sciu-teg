97142c40258a143bd4aba8e0668222e050846d79
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-23 20:20:47 -0400

    feat: group sold products report by category

--- components/reports/SoldProductsReport.tsx
+++ components/reports/SoldProductsReport.tsx
@@ -1,30 +1,52 @@
-import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Table, TableContainer, Tbody, Td, Th, Tr } from '@chakra-ui/react'
+import { groupBy } from 'lodash'
+import { Fragment } from 'react'
 import { ProductReport } from 'types/report'
 
 export const SoldProductsReport = ({ data }: { data: ProductReport[] }) => {
+  const groupedData = groupBy(data, 'category.name')
+
   return (
     <>
       <TableContainer>
         <Table>
-          <Thead>
-            <Tr>
-              <Th>Producto</Th>
-              <Th textAlign="center">Cantidad</Th>
-              <Th isNumeric>Monto</Th>
-            </Tr>
-          </Thead>
           <Tbody>
-            {data.map((product) => (
-              <Tr key={product.id}>
-                <Td>{product.name}</Td>
-                <Td textAlign="center">{product.quantity}</Td>
-                <Td isNumeric>$ {product.amount.toFixed()}</Td>
-              </Tr>
+            {Object.keys(groupedData).map((key) => (
+              <Fragment key={key}>
+                <Tr key={key}>
+                  <Th scope="rowgroup" color="black" _dark={{ color: 'white' }} pb={2} pt={6}>
+                    {key}
+                  </Th>
+                  <Th textAlign="center" pb={2} pt={6} scope="colgroup">
+                    Cantidad
+                  </Th>
+                  <Td textAlign="right" fontWeight="medium" pb={2} pt={6}>
+                    Total: $ {groupedData[key].reduce((ac, c) => ac + c.amount, 0)}
+                  </Td>
+                </Tr>
+
+                {groupedData[key].map((row, index) => (
+                  <Tr key={index}>
+                    <Td pl={10} py={2}>
+                      <i>{row.name}</i>
+                    </Td>
+                    <Td textAlign="center">{row.quantity}</Td>
+                    <Td isNumeric py={2}>
+                      $ {row.amount}
+                    </Td>
+                  </Tr>
+                ))}
+              </Fragment>
             ))}
 
             <Tr>
               <Td colSpan={3} textAlign="right" fontWeight="medium">
-                TOTAL GENERAL: $ {data.reduce((ac, product) => ac + product.amount, 0)}
+                TOTAL GENERAL: ${' '}
+                {Object.values(groupedData).reduce(
+                  (grandTotal, payMethod) =>
+                    grandTotal + payMethod.reduce((total, current) => total + current.amount, 0),
+                  0
+                )}
               </Td>
             </Tr>
           </Tbody>
