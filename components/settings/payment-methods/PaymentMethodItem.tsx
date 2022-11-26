78e749e1adbfa927e2634f1a889caa135030aa14
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-26 19:04:37 -0400

    feat: add more info to payment methods in payment methods list

--- components/settings/payment-methods/PaymentMethodItem.tsx
+++ components/settings/payment-methods/PaymentMethodItem.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react'
+import { chakra, Divider, Flex, Text } from '@chakra-ui/react'
 import { SimpleBox } from 'components/app'
 import { useAuth } from 'hooks'
 import { PaymentMethodWithCurrencies } from 'types/paymentMethod'
@@ -12,11 +12,23 @@ export const PaymentMethodItem = ({
   const { user } = useAuth()
 
   return (
-    <SimpleBox as="li" display="flex" justifyContent="space-between">
-      <Text fontWeight="bold">{paymentMethod.name}</Text>
-      {user?.permissions.includes('EDIT_PAYMENTMETHOD') && (
-        <EditPaymentMethodModal paymentMethod={paymentMethod} />
-      )}
+    <SimpleBox as="li">
+      <Flex justifyContent="space-between">
+        <Text fontWeight="bold">{paymentMethod.name}</Text>
+        {user?.permissions.includes('EDIT_PAYMENTMETHOD') && (
+          <EditPaymentMethodModal paymentMethod={paymentMethod} />
+        )}
+      </Flex>
+      <Divider my={2} />
+      <Text>{paymentMethod.description}</Text>
+      <Divider my={2} />
+      <Text>Monedas: {paymentMethod.currencies.map((c) => c.symbol).join(' | ')}</Text>
+      <Text>
+        Datos adicionales: {paymentMethod.metaPayment?.map((mp) => mp.name).join(' | ')}
+        {!paymentMethod.metaPayment?.length && (
+          <chakra.span fontStyle="italic">ninguno</chakra.span>
+        )}
+      </Text>
     </SimpleBox>
   )
 }
