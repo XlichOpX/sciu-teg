d88db7264ee4bd20268060ce910f132966b1e1a4
kurokuro15 <kurokuro15000@gmail.com>
2022-11-26 21:55:37 -0400

    Fix: Resolve some errors of encrypting and rework the result of encrypt functions see JsDoc

--- lib/crypter/index.ts
+++ lib/crypter/index.ts
@@ -3,29 +3,34 @@ import CryptoJS from 'crypto-js'
 const { SECRET } = process.env
 
 /**
- *  Encrypt function, return an array of WordArray type in the index 0 the string hashed with *SHA-256*
- *  and the index 1 the hash *SHA-256* hashed to *hmac-sha512* with secret
+ *  Encrypt function, return an string hashed with *SHA-256*
  * @param msg String that to hash
- * @returns WordArray[] with hash and hash hashed
+ * @returns string with hash
  */
-export function encrypt(msg: string): string[] {
+export function encrypt(msg: string): string {
+  const hashed = CryptoJS.SHA256(msg).toString()
+  // pure text , SECRET text
+  return hashed
+}
+/**
+ *  Encrypt function, return an string hashed with *HmacSHA256*
+ * @param msg String that to hash with HmacSHA256
+ * @returns string with hash hashed
+ */
+export function encryptToSaveDB(msg: string): string {
   if (!SECRET) throw new Error('Not SECRET encrypt environment variables configured')
   const k = SECRET
-  const hashed = CryptoJS.SHA256(msg).toString()
-  const hmached = CryptoJS.HmacSHA256(hashed, k).toString()
-  // pure text , SECRET text
-  return [hashed, hmached]
+  return CryptoJS.HmacSHA256(encrypt(msg), k).toString()
 }
-
 /**
  * Compare function, return true or false between a string and a *SHA-256* HASH,
  * @param toCompare string
- * @param msg string
+ * @param encrypted string
  * @returns Boolean
  */
-export function compare(toCompare: string, msg: string): boolean {
-  const comparer = encrypt(toCompare)[1]
-  return comparer === msg ? (msg === comparer ? true : false) : false
+export function compare(toCompare: string, encrypted: string): boolean {
+  const comparer = encryptToSaveDB(toCompare)
+  return comparer === encrypted
 }
 
 export function secretCrypt(secret: Prisma.SecretCreateInput) {
@@ -33,8 +38,10 @@ export function secretCrypt(secret: Prisma.SecretCreateInput) {
     if (Object.prototype.hasOwnProperty.call(secret, key)) {
       const property = secret[key as keyof Prisma.SecretCreateInput]
       if (key.includes('answer')) {
-        const encrypted = typeof property === 'string' ? encrypt(property) : []
-        secret[key as keyof Prisma.SecretCreateInput] = encrypted[1]
+        if (typeof property === 'string') {
+          const encrypted = encrypt(property)
+          secret[key as keyof Prisma.SecretCreateInput] = encrypted
+        }
       }
     }
   }
