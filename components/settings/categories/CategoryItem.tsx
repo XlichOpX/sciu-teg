eda3563c1dafa2d80cbd5d1ba050bb5a79c22097
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-29 12:17:12 -0400

    fix: refactor useCategories and handle conflict error

--- components/settings/categories/CategoryItem.tsx
+++ components/settings/categories/CategoryItem.tsx
@@ -2,26 +2,14 @@ import { Divider, Text } from '@chakra-ui/react'
 import { Category } from '@prisma/client'
 import { SimpleBox } from 'components/app'
 import { useAuth } from 'hooks'
-import { SubmitHandler } from 'react-hook-form'
-import { CategoryInput } from 'types/category'
 import { EditCategoryModal } from './EditCategoryModal'
 
-export const CategoryItem = ({
-  category,
-  onDelete,
-  onUpdate
-}: {
-  category: Category
-  onUpdate: SubmitHandler<CategoryInput>
-  onDelete: () => Promise<void>
-}) => {
+export const CategoryItem = ({ category }: { category: Category }) => {
   const { user } = useAuth()
 
   return (
     <SimpleBox pos="relative">
-      {user?.permissions.includes('EDIT_CATEGORY') && (
-        <EditCategoryModal defaultValues={category} onSubmit={onUpdate} onDelete={onDelete} />
-      )}
+      {user?.permissions.includes('EDIT_CATEGORY') && <EditCategoryModal category={category} />}
       <Text fontWeight="bold">{category.name}</Text>
       <Divider my={2} />
       <Text>{category.description}</Text>
--- hooks/useCategories.ts
+++ hooks/useCategories.ts
@@ -1,43 +1,11 @@
-import { useToast } from '@chakra-ui/react'
 import { Category } from '@prisma/client'
 import { HttpError } from 'lib/http-error'
 import { useMemo, useState } from 'react'
-import {
-  createCategory as createCategorySv,
-  deleteCategory as deleteCategorySv,
-  updateCategory as updateCategorySv
-} from 'services/categories'
 import useSWR from 'swr'
-import { CategoryInput } from 'types/category'
 
 export const useCategories = () => {
   const [search, setSearch] = useState('')
-  const { data, error, mutate } = useSWR<Category[], HttpError>('/api/category')
-  const toast = useToast()
-
-  const createCategory = async (data: CategoryInput) => {
-    await createCategorySv(data)
-    await mutate()
-    toast({ status: 'success', description: 'Categoría creada' })
-  }
-
-  const updateCategory = async (id: number, data: CategoryInput) => {
-    await updateCategorySv(id, data)
-    await mutate()
-    toast({ status: 'success', description: 'Categoría actualizada' })
-  }
-
-  const deleteCategory = async (id: number) => {
-    try {
-      await deleteCategorySv(id)
-      await mutate()
-      toast({ status: 'success', description: 'Categoría eliminada' })
-    } catch (error) {
-      if (error instanceof Error) {
-        toast({ status: 'error', description: 'No se pudo eliminar la categoría' })
-      }
-    }
-  }
+  const { data, error } = useSWR<Category[], HttpError>('/api/category')
 
   const categories = useMemo(
     () => data?.filter((c) => c.name.toLocaleLowerCase().includes(search)),
@@ -48,9 +16,6 @@ export const useCategories = () => {
     categories,
     error,
     isLoading: !data && !error,
-    createCategory,
-    updateCategory,
-    deleteCategory,
     search,
     setSearch,
     errorMsg: error
@@ -60,3 +25,5 @@ export const useCategories = () => {
       : undefined
   }
 }
+
+export const categoryKeysMatcher = '^/api/category.*'
