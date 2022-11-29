8bed330c50bd6d210a33df98cefe7495ecd276ba
Yhan Montaño <yhan.carlos2001@gmail.com>
2022-11-29 14:56:16 -0400

    fix: handle conflict on payment method delete

--- services/paymentMethods.ts
+++ services/paymentMethods.ts
@@ -1,4 +1,5 @@
 import { fetch } from 'lib/fetch'
+import { HttpError } from 'lib/http-error'
 import { PaymentMethodCreateInput } from 'types/paymentMethod'
 
 export async function updatePaymentMethod(id: number, data: PaymentMethodCreateInput) {
@@ -13,5 +14,12 @@ export async function createPaymentMethod(data: PaymentMethodCreateInput) {
 }
 
 export async function deletePaymentMethod(id: number) {
-  return await fetch(`/api/paymentMethod/${id}`, { method: 'DELETE' })
+  try {
+    return await fetch(`/api/paymentMethod/${id}`, { method: 'DELETE' })
+  } catch (error) {
+    if (error instanceof HttpError && error.statusCode === 409) {
+      error.message = 'El método de pago se encuentra en uso'
+    }
+    throw error
+  }
 }
