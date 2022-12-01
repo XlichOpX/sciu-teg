2d3c07662d6369aa55df00ac6eaacba235212286
kurokuro15 <kurokuro15000@gmail.com>
2022-12-01 08:14:43 -0400

    Fix: toFixed in 'total recaudado'

--- components/home/MonthlySoldProducts.tsx
+++ components/home/MonthlySoldProducts.tsx
@@ -32,7 +32,8 @@ export const MonthlySoldProducts = () => {
           </Heading>
           <Divider my={2} />
           <Text textAlign="center" fontWeight="medium">
-            Total recaudado: ${(data as ProductReport[]).reduce((ac, c) => ac + c.amount, 0)}
+            Total recaudado: $
+            {(data as ProductReport[]).reduce((ac, c) => ac + c.amount, 0).toFixed(2)}
           </Text>
           <SoldProductsGraph data={data as ProductReport[]} />
         </>
